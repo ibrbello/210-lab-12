b0b8a89fa09067ebc298fba07d1ed9592575f14a
Ibrahim Bello <ibrbello10@gmail.com>
2026-02-22 19:25:49 -0800

    Lab complete

--- main.cpp
+++ main.cpp
@@ -19,10 +19,36 @@ int main() {
     if (fin.good()) {
         for ( size_t i = 0; i < sales.size(); i++) {
             fin >> sales[i];
-            cout << sales[i] << endl;
+            // Check that file was read correctly
+            cout << sales[i] << " "; 
         }
         fin.close();
     }
     else   
         cout << "File not found. \n";
+    cout << endl;
+    // Operations: find max, min, sum, find, sort
+    // Max, min, & sum
+    cout << "1. Max: " << *max_element(sales.begin(), sales.end()) << endl;
+    cout << "2. Min: " << *min_element(sales.begin(), sales.end()) << endl;
+    cout << "3. Sum: " << accumulate(sales.begin(), sales.end(), 0) << endl;
+
+    // find
+    double target = 3.4;   // search target
+    array<double, SIZE>::iterator it;  // declare iterator to point to the found element
+    it = find(sales.begin(), sales.end(), target);
+    cout << "4. Value " << target;
+    if (it != sales.end())
+        cout << " found in position " << it - sales.begin() << endl;
+    else
+        cout << " was not found.\n";
+    cout << "    Value: " << *it << endl;
+
+    // Sort
+    sort(sales.begin(), sales.end());
+    cout << "5.  Sorted: ";
+    for (double val : sales) cout << val << " "; cout << endl;
+    return 0;
+
+
 }
